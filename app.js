10f02b814409a1fabf0dcbd27ba55e123ec3b56f
jimmyctw <66037440+jimmyctw@users.noreply.github.com>
2021-03-10 08:53:24 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,24 +3,32 @@ const enBtn = document.getElementById('enBtn');
 const resumeEn = document.getElementById('resumeEn');
 const resumeJp = document.getElementById('resumeJp');
 const mainIntro = document.getElementById('mainIntro');
-const imgBtn = document.getElementsByClassName('img-btn');
-const imgBox = document.getElementsByClassName('img-box');
 
 
- enBtn.addEventListener('click',
-    ()=>{
-        resumeJp.classList.remove('displayNone')
-        resumeEn.classList.remove('displayNone')
-        mainIntro.classList.add('displayNone')
-        resumeJp.classList.toggle('displayNone')
-    }
-)
-jpBtn.addEventListener('click',
-    ()=>{
-        resumeJp.classList.remove('displayNone')
-        resumeEn.classList.remove('displayNone')
-        mainIntro.classList.add('displayNone')
-        resumeEn.classList.toggle('displayNone')
-    }
-)
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    enBtn.addEventListener('click',
+        ()=>{
+            resumeJp.classList.remove('displayNone')
+            resumeEn.classList.remove('displayNone')
+            mainIntro.classList.add('displayNone')
+            resumeJp.classList.toggle('displayNone')
+        }
+    )
+    jpBtn.addEventListener('click',
+        ()=>{
+            resumeJp.classList.remove('displayNone')
+            resumeEn.classList.remove('displayNone')
+            mainIntro.classList.add('displayNone')
+            resumeEn.classList.toggle('displayNone')
+        }
+    )
+    
+});
+
+
+
+
+
 
