80e29b39eb8321674a97686b1437f3ce1e1350b2
jimmyctw <66037440+jimmyctw@users.noreply.github.com>
2021-03-10 01:41:09 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-$("#navEdu").innerhtml = ''
+const jpBtn = document.getElementById('jpBtn');
+const enBtn = document.getElementById('enBtn');
+const resumeEn = document.getElementById('resumeEn');
+const resumeJp = document.getElementById('resumeJp');
+const mainIntro = document.getElementById('mainIntro');
+const imgBtn = document.getElementsByClassName('img-btn');
+const imgBox = document.getElementsByClassName('img-box');
+
+
+jpBtn.addEventListener('click',
+    ()=>{
+        mainIntro.classList.add('displayNone')
+        resumeJp.classList.toggle('displayNone')
+    }
+)
+enBtn.addEventListener('click',
+    ()=>{
+        mainIntro.classList.add('displayNone')
+        resumeEn.classList.toggle('displayNone')
+    }
+)
+
