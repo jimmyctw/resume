33ee741132207198b0820ad7aea404c2cae35991
jimmyctw <66037440+jimmyctw@users.noreply.github.com>
2021-03-15 01:35:31 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,93 +1,34 @@
-const serviceBtns = document.getElementsByClassName('serviceBtn');
-const formBox = document.getElementById('formBox')
-const headerImg = document.getElementById('headerImg')
-const headerChangeSet = setInterval(headerChange, 2000) 
-const serviceImgSlideSet = setInterval(serviceImgSlide, 2000)
-const formAlert = ()=> {alert('すべてのフィールドにちゃんと入力してください!!');}
-let imgNr = 1;
+const jpBtn = document.getElementById('jpBtn');
+const enBtn = document.getElementById('enBtn');
+const resumeEn = document.getElementById('resumeEn');
+const resumeJp = document.getElementById('resumeJp');
+const mainIntro = document.getElementById('mainIntro');
 
 
 
 
-function serviceImgSlide(){
-    const serviceItem = document.getElementById('serviceItem');
-    let img = `<img src="/assets/service/service${imgNr}.png" alt="company image">`
-    serviceItem.innerHTML = img;
-    imgNr ++;
-    if(imgNr > 5){
-        imgNr = 1;
-    }
-    // console.log(serviceItem)
-}
-
-
-
-
-function headerChange(){
-    const jp = document.getElementById("headerJp")
-    const en = document.getElementById("headerEn")
-    jp.classList.toggle('displayNone')
-    en.classList.toggle('displayNone')
-}
-        
-
-
-
-for( let i = 0 ; i < serviceBtns.length ; i++ ){
-    const serviceBtn = serviceBtns[i];
-    serviceBtn.addEventListener('click', () => {
-        const serviceDetail = serviceBtn.nextElementSibling;
-        const right = serviceBtn.firstElementChild;
-        const down = serviceBtn.firstElementChild.nextElementSibling;
-        right.classList.toggle('displayNone')
-        down.classList.toggle('displayNone')    
-        serviceDetail.classList.toggle("displayNone");
-    })
-}
+document.addEventListener("DOMContentLoaded", function(){
+    enBtn.addEventListener('click',
+        ()=>{
+            resumeJp.classList.remove('displayNone')
+            resumeEn.classList.remove('displayNone')
+            mainIntro.classList.add('displayNone')
+            resumeJp.classList.toggle('displayNone')
+        }
+    )
+    jpBtn.addEventListener('click',
+        ()=>{
+            resumeJp.classList.remove('displayNone')
+            resumeEn.classList.remove('displayNone')
+            mainIntro.classList.add('displayNone')
+            resumeEn.classList.toggle('displayNone')
+        }
+    )
+    
+});
 
 
 
 
 
 
-
-
-
-submitBtn.addEventListener('click', (event)=>{
-    event.preventDefault();
-    const fname = document.getElementById('fname').value;
-    const fmail = document.getElementById('fmail').value;
-    const fmobile = document.getElementById('fmobile').value;
-    const fmessage = document.getElementById('fmessage').value;
-
-    if(fname !== '' && fmail !== '' && fmobile !== '' && fmessage !== ''){
-        confirmContainerBuild(fname, fmail, fmobile, fmessage);
-    }else{
-        formAlert();
-    }
-})
-
-function confirmContainerBuild(fname, fmail, fmobile, fmessage){
-    const formAndSend = document.getElementById('formAndSend');
-    const confirmContainer = document.createElement('div')
-    confirmContainer.classList.add('confirmContainer');
-    const confirmCotent = document.createElement('div')
-    confirmCotent.classList.add('confirmCotent');
-    confirmCotent.setAttribute('id', 'sendContent');
-    const confirmCotentVal = `
-        <span>Your message</span>
-        <span> ${fname}</span>
-        <span> Email: ${fmail}</span>
-        <span> Mobile: ${fmobile}</span>
-        <p> Message: ${fmessage}</p>
-    `
-    confirmCotent.innerHTML = confirmCotentVal;
-    const sendContentBtn = document.createElement('button');
-    sendContentBtn.classList.add('sendContentBtn')
-    sendContentBtn.innerHTML = "send";
-    confirmContainer.appendChild(confirmCotent)
-    confirmContainer.appendChild(sendContentBtn)
-    formAndSend.appendChild(confirmContainer)
-    formBox.classList.add('displayNone')
-
-}
