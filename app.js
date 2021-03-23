6800b5804cc2d07069ba1e75c5e2b25849de1dfa
jimmyctw <66037440+jimmyctw@users.noreply.github.com>
2021-03-23 01:39:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const enBtn = document.getElementById('enBtn');
 const resumeEn = document.getElementById('resumeEn');
 const resumeJp = document.getElementById('resumeJp');
 const mainIntro = document.getElementById('mainIntro');
+const introSvg = document.getElementById('introSvg');
+const redCtrl = document.getElementById('redCtrl');
+const greenCtrl = document.getElementById('greenCtrl');
+const blueCtrl = document.getElementById('redCtrl');
+
+
+
+
+
 
 
 
@@ -10,25 +19,31 @@ const mainIntro = document.getElementById('mainIntro');
 document.addEventListener("DOMContentLoaded", function(){
     enBtn.addEventListener('click',
         ()=>{
-            resumeJp.classList.remove('displayNone')
-            resumeEn.classList.remove('displayNone')
-            mainIntro.classList.add('displayNone')
-            resumeJp.classList.toggle('displayNone')
+            resumeJp.classList.remove('displayNone');
+            resumeEn.classList.remove('displayNone');
+            mainIntro.classList.add('displayNone');
+            resumeJp.classList.toggle('displayNone');
         }
     )
     jpBtn.addEventListener('click',
         ()=>{
-            resumeJp.classList.remove('displayNone')
-            resumeEn.classList.remove('displayNone')
-            mainIntro.classList.add('displayNone')
-            resumeEn.classList.toggle('displayNone')
+            resumeJp.classList.remove('displayNone');
+            resumeEn.classList.remove('displayNone');
+            mainIntro.classList.add('displayNone');
+            resumeEn.classList.toggle('displayNone');
         }
     )
-    
+
 });
 
 
-
+function changeColor(){
+    const redCtrlVal= redCtrl.value;
+    const greenCtrlVal= greenCtrl.value;
+    const blueCtrlVal= blueCtrl.value;
+    const rgb = `rgb(${redCtrlVal},${greenCtrlVal},${blueCtrlVal})`
+    introSvg.setAttribute("fill", rgb)
+}
 
 
 
